ffd27e4463fbf807b52b567b1efb6c2c4a639957
Shivam seth <shivamseth221104@gmail.com>
2023-05-02 18:20:00 +0530

    as

---
+++ App1.js
@@ -0,0 +1,37 @@
+ //   const heading = React.createElement(
+    //     "h1",
+    //     {
+    //       id: "title",
+    //     },
+    //     "Namstey everyone"
+    //   );
+
+
+    //   root.render(heading);
+    const heading1 = React.createElement(
+        "h1",
+        {
+          id: "title",
+        },
+        "Heading1"
+      );
+  
+      const heading2 = React.createElement(
+        "h2",
+        {
+          id: "title",
+        },
+        "Heading2"
+      );
+      const container = React.createElement(
+        "div",
+        {
+          id: "container",
+        },
+        [heading1, heading2]
+      );
+  
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  
+      root.render(container); 
